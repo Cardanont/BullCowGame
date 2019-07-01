1c6bddab2333d6f6a0d6bc7bbf47911b02e2aed1
Daniel Ontiveros <cardanont@gmail.com>
2019-06-30 23:30:43 -0500

    Add project files.

---
+++ BullsAndCows.cpp
@@ -0,0 +1,11 @@
+// BullsAndCows.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Wellcome to Bulls and Cows\n";
+}
