111c7d5623e65c833127a7c79b2c2d363ae557d1
Daniel Ontiveros <cardanont@gmail.com>
2019-07-01 01:37:56 -0500

    Adding some functions

--- BullsAndCows.cpp
+++ BullsAndCows.cpp
@@ -2,10 +2,42 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void PrintIntro();
+void GetGuessAndPrintBack();
+
 int main()
 {
-    cout << "Wellcome to Bulls and Cows\n";
+
+	PrintIntro();
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+
+	cout << endl;
+
+	return 0;
+}
+
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 9;
+	cout << "Wellcome to Bulls and Cows\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogam I'm thinking of? \n";
+
+}
+
+void GetGuessAndPrintBack()
+{
+	// Get guess from player
+
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	// Repeat the guess back to them
+	cout << "Your Guess was: " << Guess << endl;
 }
